84d1b1af8e5a1b99f9bec020fb9c7633f3087769
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-05-29 08:53:14 -0500

    :recycle: update to utilize morestrings local package

--- gowiki/how-to-write-go-code/example/user/hello/hello.go
+++ gowiki/how-to-write-go-code/example/user/hello/hello.go
@@ -1,7 +1,11 @@
 package main 
 
-import "fmt" 
+import (
+    "fmt" 
+    "example/user/hello/morestrings"
+)
 
 func main() {
     fmt.Println("Hello, world!")
+    fmt.Println(morestrings.ReverseRunes("!oG,olleH"))
 }
